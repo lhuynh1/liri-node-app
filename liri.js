5e8c5502ac79bfb84f00938e93a05d2540790a05
Lisa Huynh <lisaahuynh@yahoo.com>
2018-04-20 21:31:08 -0400

    added twitter function

--- liri.js
+++ liri.js
@@ -26,17 +26,36 @@ switch (dataValue) {
 
 // twitter function to show last 20 tweets
 function myTweets() {
+  // twitter api client with keys from keys.js
   var client = new Twitter(keys.twitter);
-}
-
-
-
-var spotify = new Spotify(keys.spotify);
-// spotify api logic
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+  // setting parameters for twitter
+  var params = {
+    screen_name: 'HuynhLisa',
+    count: 20
+  };
+  client.get('statuses/user_timeline', params, function(err, tweets, response) {
+    // console.log(response);
+    console.log('Last 20 tweets:');
+    if (!err) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(`Created on: ${tweets[i].created_at}`);
+        // spacing between each tweet for readability
+        console.log(' ');
+        console.log(`Tweet: ${tweets[i].text}`);
+      }
     }
+  });
+
+};
+
+
+
+// var spotify = new Spotify(keys.spotify);
+// // spotify api logic
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
    
-  console.log(data); 
-  }); 
+//   console.log(data); 
+//   }); 
